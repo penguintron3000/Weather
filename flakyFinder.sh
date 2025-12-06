2d7c3326ae7e37c3145ca1c7803cc3835e643b3e
Lucas Eby <lucaseby@outlook.com>
2025-12-06 10:40:32 -0600

    Added bash script to check for flaky tests

---
+++ flakyFinder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ITERATIONS=100
+LOGFILE="test_output.log"
+for ((i=1; i<=ITERATIONS; i++)); do
+	~/Library/Android/sdk/platform-tools/adb shell pm clear edu.uiuc.cs427app >> "$LOGFILE" 2>&1
+	./gradlew :app:connectedAndroidTest --info >> "$LOGFILE" 2>&1
+	./gradlew :app:test --rerun-tasks >> "$LOGFILE" 2>&1
+done
+
+PATTERN=' > .* FAILED$|Tests on .* failed|FAILURE: Build failed with an exception'
+if rg -n "$PATTERN" "$LOGFILE"; then
+    echo "" | tee -a "$LOGFILE"
+    echo "!!! FAILURES FOUND !!!"
+else
+    echo "No failures found"
+fi
